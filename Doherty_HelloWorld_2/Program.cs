8a84eee094cc11a5cc0123387a7983d4dc63e37a
smd2566 <smd2566@rit.edu>
2021-08-26 19:20:22 -0400

    Submitting "Hello World" Assignment

---
+++ Doherty_HelloWorld_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Doherty_HelloWorld_2
+{
+
+    //Author: Shane Doherty
+    //Purpose: To print "Hello World" and "Shane Doherty"
+    //Restrictions / Known Errors: None
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Shane Doherty");
+        }
+    }
+}
+
