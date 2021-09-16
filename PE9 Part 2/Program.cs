084aaca2819d0271e7f378e9fae285ea70e00b2c
smd2566 <smd2566@rit.edu>
2021-09-15 21:36:20 -0400

    Commit 1

---
+++ PE9 Part 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE9_Part_2
+{
+    //Author: Shane Doherty
+    //Purpose: To initiate ReadLine() from a delegate method
+    //Restrictions: None
+    class Program
+    {
+        //Author: Shane Doherty
+        //Purpose: Encapsulates the method
+        //Restrictions: None
+        public delegate void Del();
+
+        //Author: Shane Doherty
+        //Purpose: Mimics ReadLine()
+        //Restrictions: None
+        public static void DelegateMethod()
+        {
+            Console.ReadLine();
+        }
+
+        //Author: Shane Doherty
+        //Purpose: Main - Calls the delegate method
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            Del handler = DelegateMethod;
+            handler();
+
+
+
+
+
+        }
+    }
+}
