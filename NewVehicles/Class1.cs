4bdf05f3873e1bd9e8deeb763430548f221b012b
smd2566 <smd2566@rit.edu>
2021-09-27 21:55:18 -0400

    First Commit - LoadPassenger has errors because of language version

---
+++ NewVehicles/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Vehicles
+{
+    public abstract class Vehicle
+    {
+        public virtual void LoadPassenger() { }
+
+    }
+
+    public abstract class Car : Vehicle
+    {
+
+    }
+
+    public abstract class Train : Vehicle
+    {
+
+    }
+
+    public interface PassengerCarrier
+    {
+        public void LoadPassenger() { }
+    }
+
+    public interface HeavyLoadCarrier
+    {
+
+    }
+
+    public class Compact : Car, PassengerCarrier
+    {
+
+    }
+
+    public class SUV : Car, PassengerCarrier
+    {
+
+    }
+
+    public class Pickup : Car, HeavyLoadCarrier
+    {
+
+    }
+
+    public class PassengerTrain : Train, PassengerCarrier
+    {
+
+    }
+
+    public class FreightTrain : Train, HeavyLoadCarrier
+    {
+
+    }
+
+    public class _424DoubleBogey : Train, HeavyLoadCarrier
+    {
+
+    }
+
+}
---
+++ Traffic/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewVehicles;
+
+namespace Traffic
+{
+    class Program
+    {
+        static public void AddPassenger(object obj)
+        {
+            if (obj is PassengerCarrier)
+            {
+                PassengerCarrier passengerCarrier;
+                passengerCarrier = (PassengerCarrier)obj;
+
+                passengerCarrier.LoadPassenger();
+                Console.WriteLine(passengerCarrier.ToString());
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            SUV Subaru = new SUV();
+            AddPassenger(Subaru);
+        }
+    }
+}
