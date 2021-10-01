80c9661cd0fd978a92b4d0fd7a1ea28e5b2e2fca
smd2566 <smd2566@rit.edu>
2021-09-30 20:05:21 -0400

    First commit: Added comments

---
+++ PE12/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE12
+{
+    //Author: Shane Doherty
+    //Purpoose: Program - Returns the private string from the base class and appends a new phrase
+    //Restrictions: None
+    class Program
+    {
+        public class MyClass
+        {
+            private string myString = "Hello";
+            private string MyString
+            {
+                set
+                {
+                    
+                }
+            }
+
+            //returns myString
+            public virtual string GetString()
+            {
+                return myString;
+            }
+        }
+
+        //Returns the string to the user and appends (output derived from the base class)
+        public class MyDerivedClass: MyClass
+        {
+            //overrides base GetString method
+            public override string GetString()
+            {
+                return base.GetString() + " (output derived from the base class)";
+            }
+        }
+
+
+
+
+        //Author: Shane Doherty
+        //Purpoose: Main - Creates an instance of the derived class and calls GetString() through that instance
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+           
+            MyDerivedClass myDerivedClass = new MyDerivedClass();
+            Console.WriteLine(myDerivedClass.GetString());
+
+        }
+    }
+}
