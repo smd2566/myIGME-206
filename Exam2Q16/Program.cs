11046a734bb6b22cc5be4c8c9153e36871c9e3ff
smd2566 <smd2566@rit.edu>
2021-10-21 22:00:23 -0400

    First commit - Added comments

---
+++ Exam2Q16/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam2Q16
+{
+    //Author: Shane Doherty
+    //Purpose: To create a shallow copy of an object
+    //Restrictions: None
+    class Program
+    {
+
+        //performs the shallow copy
+        public class MyClass
+        {
+            public object ShallowCopy()
+            {
+                return this.MemberwiseClone();
+            }
+        }
+
+
+        //Author: Shane Doherty
+        //Purpose: Main: Changes objectB into a shallow copy of objectA
+        //Restrictions: None
+        static void Main(string[] args)
+        {
+            MyClass objectA = new MyClass();
+            MyClass objectB = (MyClass)objectA.ShallowCopy();
+
+        }
+    }
+}
